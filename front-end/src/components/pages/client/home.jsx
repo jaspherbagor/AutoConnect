b5f50883711ed89a60c24880ecac5e2eece90bef
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-31 15:48:25 +0800

    updated the contents of the hero section

--- front-end/src/components/pages/client/home.jsx
+++ front-end/src/components/pages/client/home.jsx
@@ -9,11 +9,11 @@ const Home = () => {
         <section className="hero-section container-fluid px-md-4 px-sm-4 px-2 py-5">
             <div className="row">
                 <div className="col-md-6 d-flex align-items-center justify-content-center">
-                    <div className="container">
-                        <h2 className="fw-bolder mb-4">On-Demand Car Repairs, Anytime, Anywhere</h2>
+                    <div className="container-fluid text-start">
+                        <h2 className="mb-4 display-5 fw-bolder">On-Demand Car Repairs, Anytime, Anywhere</h2>
                         <h5 className="mb-4">Stuck on the Road? We'll Fix It Wherever You Are.</h5>
                         <a href="">
-                            <button type="button" className="btn btn-success btn-outline-danger fw-semibold py-2 px-3">Get Help Now</button>
+                            <button type="button" className="btn btn-success btn-outline-danger fw-semibold py-2 px-3 fs-5">GET HELP NOW</button>
                         </a>
                     </div>
                 </div>
