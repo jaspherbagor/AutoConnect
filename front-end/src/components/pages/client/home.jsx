f9e29aac82beca97402ec0078fa7eb8b40e206ed
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-31 14:12:35 +0800

    added initial contents in the hero section and added link and script cdn of bootstrap

--- front-end/src/components/pages/client/home.jsx
+++ front-end/src/components/pages/client/home.jsx
@@ -6,8 +6,22 @@ const Home = () => {
     <div>
         <ClientNavbar />
 
-        <p>This is a home page</p>
-        
+        <section className="hero-section container-fluid px-md-4 px-sm-4 px-2 py-5">
+            <div className="row">
+                <div className="col-md-6 d-flex align-items-center justify-content-center">
+                    <div className="container">
+                        <h2 className="fw-bolder">Repairing Autos</h2>
+                        <h5>Always active 24/7</h5>
+                    </div>
+                </div>
+                <div className="col-md-6 d-flex align-items-center justify-content-center">
+                    <p>This is an image</p>
+                    <img src="" alt="" />
+                    
+                </div>
+            </div>
+        </section>
+
         <ClientFooter />
     </div>
   )
