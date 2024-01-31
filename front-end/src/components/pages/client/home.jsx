6d7bc7ac0d41b17a2f02da953b825283c1b5bbe8
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-31 16:10:06 +0800

    replaced the file type of the hero image and added styles to the hero section

--- front-end/src/components/pages/client/home.jsx
+++ front-end/src/components/pages/client/home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ClientNavbar from '../../layouts/client/navbar';
 import ClientFooter from '../../layouts/client/footer';
+import '../../styles/client/home.css';
 const Home = () => {
   return (
     <div>
@@ -10,15 +11,15 @@ const Home = () => {
             <div className="row">
                 <div className="col-md-6 d-flex align-items-center justify-content-center">
                     <div className="container-fluid text-start">
-                        <h2 className="mb-4 display-5 fw-bolder">On-Demand Car Repairs, Anytime, Anywhere</h2>
+                        <h2 className="mb-4 display-5 fw-bold">On-Demand Car Repairs, Anytime, Anywhere</h2>
                         <h5 className="mb-4">Stuck on the Road? We'll Fix It Wherever You Are.</h5>
                         <a href="">
-                            <button type="button" className="btn btn-success btn-outline-danger fw-semibold py-2 px-3 fs-5">GET HELP NOW</button>
+                            <button type="button" className="btn btn-success btn-outline-danger fw-semibold py-3 px-3 fs-5 get-help-btn">GET HELP NOW</button>
                         </a>
                     </div>
                 </div>
                 <div className="col-md-6 d-flex align-items-center justify-content-center">
-                    <img src="../images/hero-image.png" alt="" className="img-fluid" />            
+                    <img src="../images/hero-image.svg" alt="" className="img-fluid" />            
                 </div>
             </div>
         </section>
