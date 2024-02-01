148ca55317ce393f33ad8f910a1c390bfd84f2a7
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-01 15:32:51 +0800

    added a temporary structure of a feature section

--- front-end/src/components/pages/client/home.jsx
+++ front-end/src/components/pages/client/home.jsx
@@ -24,7 +24,70 @@ const Home = () => {
             </div>
         </section>
         <section className="feature-section container-fluid px-md-4 px-sm-4 py-5">
-
+            <div className="row">
+                <div className="col-lg-3 col-md-3 col-sm-6">
+                    <div className="row">
+                        <div className="col-md-6 d-flex justify-content-center align-items-center">
+                            <i class="bi bi-emoji-smile display-1 h-100 w-100 feature-icon"></i>
+                        </div>
+                        <div className="col-md-6 align-items-center">
+                            <div className="col-12">
+                                <p className="text-start">Satisfied Customers</p>
+                            </div>
+                            <div className="col-12">
+                                <h1 className="fw-bolder text-start">500+</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-3 col-md-3 col-sm-6">
+                    <div className="row">
+                        <div className="col-md-6 d-flex justify-content-center align-items-center">
+                            <i class="bi bi-emoji-smile display-1 h-100 w-100 feature-icon"></i>
+                        </div>
+                        <div className="col-md-6 align-items-center">
+                            <div className="col-12">
+                                <p className="text-start">Satisfied Customers</p>
+                            </div>
+                            <div className="col-12">
+                                <h1 className="fw-bolder text-start">500+</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-3 col-md-3 col-sm-6">
+                    <div className="row">
+                        <div className="col-md-6 d-flex justify-content-center align-items-center">
+                            <i class="bi bi-emoji-smile display-1 h-100 w-100 feature-icon"></i>
+                        </div>
+                        <div className="col-md-6 align-items-center">
+                            <div className="col-12">
+                                <p className="text-start">Satisfied Customers</p>
+                            </div>
+                            <div className="col-12">
+                                <h1 className="fw-bolder text-start">500+</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-3 col-md-3 col-sm-6">
+                    <div className="row">
+                        <div className="col-md-6 d-flex justify-content-center align-items-center">
+                            <i class="bi bi-emoji-smile display-1 feature-icon px-4 py-3"></i>
+                        </div>
+                        <div className="col-md-6 d-flex align-items-center justify-content-center">
+                            <div className="container-fluid p-0">
+                                <div className="col-12 py-0 my-0">
+                                    <p className="text-start py-0 my-0">Satisfied Customers</p>
+                                </div>
+                                <div className="col-12 py-0 my-0">
+                                    <h1 className="fw-bolder text-start py-0 my-0">500+</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
 
         <ClientFooter />
