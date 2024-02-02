58cc9ca05d33bdba8ce7c3b09295fdb4b28b8882
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-02 13:35:02 +0800

    added an image and a structure for the services card

--- front-end/src/components/pages/client/home.jsx
+++ front-end/src/components/pages/client/home.jsx
@@ -3,29 +3,29 @@ import ClientNavbar from '../../layouts/client/navbar';
 import ClientFooter from '../../layouts/client/footer';
 import '../../styles/client/home.css';
 const Home = () => {
-  return (
-    <div>
-        <ClientNavbar />
+    return (
+        <div>
+            <ClientNavbar />
 
-        <section className="hero-section container-fluid px-md-4 px-sm-4 px-2 py-5  d-flex align-items-center justify-content-center">
-            <div className="row mt-5">
-                <div className="col-md-6 d-flex align-items-center justify-content-center mb-md-0 mb-sm-5 mb-5">
-                    <div className="container-fluid text-start">
-                        <h2 className="mb-4 display-5 fw-bold">On-Demand Car Repairs, Anytime, Anywhere</h2>
-                        <h5 className="mb-4">Stuck on the Road? We'll Fix It Wherever You Are.</h5>
-                        <a href="">
-                            <button type="button" className="btn btn-success btn-outline-danger fw-semibold py-3 px-3 fs-5 get-help-btn">GET HELP NOW</button>
-                        </a>
+            <section className="hero-section container-fluid px-md-4 px-sm-4 px-2 py-5  d-flex align-items-center justify-content-center">
+                <div className="row mt-5">
+                    <div className="col-md-6 d-flex align-items-center justify-content-center mb-md-0 mb-sm-5 mb-5">
+                        <div className="container-fluid text-start">
+                            <h2 className="mb-4 display-5 fw-bold">On-Demand Car Repairs, Anytime, Anywhere</h2>
+                            <h5 className="mb-4">Stuck on the Road? We'll Fix It Wherever You Are.</h5>
+                            <a href="">
+                                <button type="button" className="btn btn-success btn-outline-danger fw-semibold py-3 px-3 fs-5 get-help-btn">GET HELP NOW</button>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="col-md-6 d-flex align-items-center justify-content-center">
+                        <img src="../images/hero-image.svg" alt="" className="img-fluid" />
                     </div>
                 </div>
-                <div className="col-md-6 d-flex align-items-center justify-content-center">
-                    <img src="../images/hero-image.svg" alt="" className="img-fluid" />            
-                </div>
-            </div>
-        </section>
-        <section className="feature-section container-fluid px-md-4 px-sm-4 px-2 py-5">
-            <div className="row mt-5">
-                <div className="col-lg-3 col-md-4 col-sm-6 col-12 feature-type mb-4">
+            </section>
+            <section className="feature-section container-fluid px-md-4 px-sm-4 px-2 py-5">
+                <div className="row mt-5">
+                    <div className="col-lg-3 col-md-4 col-sm-6 col-12 feature-type mb-4">
                         <div className="row gap-0">
                             <div className="col-6 d-flex justify-content-center align-items-center">
                                 <i class="bi bi-emoji-smile display-3 feature-icon px-4 py-3"></i>
@@ -93,21 +93,30 @@ const Home = () => {
                             </div>
                         </div>
                     </div>
-            </div>
-        </section>
-        <section className="services-offered container-fluid px-md-4 px-sm-4 px-2 py-5">
-            <h2 className="fw-bolder text-center"><span>SERVICES</span> OFFERED</h2>
-            <div className="row mt-5">
-                <div className="col-lg-3 col-md-3 col-sm-6">
-                    <h4>This is a service sample</h4>
                 </div>
-            </div>
+            </section>
+            <section className="services-offered container-fluid px-md-4 px-sm-4 px-2 py-5">
+                <h2 className="fw-bolder text-center"><span>SERVICES</span> OFFERED</h2>
+                <div className="row mt-5">
+                    <div className="col-lg-3 col-md-3 col-sm-6">
+                        <div className="card p-0 service-card">
+                            <img src="../images/car_repair.png" className="card-img-top img-fluid" alt="..."/>
+                            <div className="card-body">
+                                <h4 className="card-head fw-bolder">CAR MAJOR REPAIR</h4>
+                                <p className="card-text text-start">Get best Car Repair Service in anywhere in the Philippines.</p>
+                                <a href="">
+                                    <button className="btn btn-danger fw-semibold view-service-btn">View Service</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-        </section>
+            </section>
 
-        <ClientFooter />
-    </div>
-  )
+            <ClientFooter />
+        </div>
+    )
 }
 
 export default Home
