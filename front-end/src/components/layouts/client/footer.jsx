14b9a5e0cb7d6e8daf75b62d1b1028dc12fd81ba
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-03 20:26:13 +0800

    updated the contents on the footer

--- front-end/src/components/layouts/client/footer.jsx
+++ front-end/src/components/layouts/client/footer.jsx
@@ -5,11 +5,23 @@ const ClientFooter = () => {
     <div>
         <section className="footer container-fluid px-md-4 px-sm-4 px-2 py-5 bg-black">
           <div className="row">
-            <div className="col-lg-2 col-md-4 col-sm-6 col-12 text-lg-start text-md-start text-sm-start text-center">
-              col-1
+            <div className="col-lg-2 col-md-4 col-sm-6 col-12 text-lg-start text-md-start text-sm-start text-center d-flex justify-content-center align-items-center">
+              <img src="../images/logo.svg" alt="" className="img-fluid" />
             </div>
-            <div className="col-lg-2 col-md-4 col-sm-6 col-12 text-lg-start text-md-start text-sm-start text-center">
-              col-2
+            <div className="col-lg-2 col-md-4 col-sm-6 col-12 text-lg-start text-md-start text-sm-start text-center text-white">
+              <h5 className="fw-bolder mb-4">CUSTOMER SERVICE</h5>
+              <a href="" className="text-decoration-none text-white">
+                <p className="footer-link">Contact Us</p>
+              </a>
+              <a href="" className="text-decoration-none text-white">
+              <p className="footer-link">FAQs</p>
+              </a>
+              <a href="" className="text-decoration-none text-white">
+              <p className="footer-link">Privacy Policy</p>
+              </a>
+              <a href="" className="text-decoration-none text-white">
+              <p className="footer-link">Terms & Condition</p>
+              </a>
             </div>
             <div className="col-lg-2 col-md-4 col-sm-6 col-12 text-lg-start text-md-start text-sm-start text-center">
               col-3
