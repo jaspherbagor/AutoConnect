a02e2406f1dcb684815d1d200c2446f514882f74
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-03 20:17:54 +0800

    added a temporary structure of the footer elements

--- front-end/src/components/layouts/client/footer.jsx
+++ front-end/src/components/layouts/client/footer.jsx
@@ -4,7 +4,26 @@ const ClientFooter = () => {
   return (
     <div>
         <section className="footer container-fluid px-md-4 px-sm-4 px-2 py-5 bg-black">
-          
+          <div className="row">
+            <div className="col-lg-2 col-md-4 col-sm-6 col-12 text-lg-start text-md-start text-sm-start text-center">
+              col-1
+            </div>
+            <div className="col-lg-2 col-md-4 col-sm-6 col-12 text-lg-start text-md-start text-sm-start text-center">
+              col-2
+            </div>
+            <div className="col-lg-2 col-md-4 col-sm-6 col-12 text-lg-start text-md-start text-sm-start text-center">
+              col-3
+            </div>
+            <div className="col-lg-2 col-md-4 col-sm-6 col-12 text-lg-start text-md-start text-sm-start text-center">
+              col-4
+            </div>
+            <div className="col-lg-2 col-md-4 col-sm-6 col-12 text-lg-start text-md-start text-sm-start text-center">
+              col-5
+            </div>
+            <div className="col-lg-2 col-md-4 col-sm-6 col-12 text-lg-start text-md-start text-sm-start text-center">
+              col-6
+            </div>
+          </div>
         </section>
     </div>
   )
