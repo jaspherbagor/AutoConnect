5ef5398151bcba5bb1735e114ed6ea8ae78c1c3d
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-03 18:59:16 +0800

    updated the heading of the services card

--- front-end/src/components/pages/client/home.jsx
+++ front-end/src/components/pages/client/home.jsx
@@ -126,7 +126,7 @@ const Home = () => {
                         <div className="card p-0 service-card mb-4">
                             <img src="../images/car_repair.png" className="card-img-top img-fluid" alt="..."/>
                             <div className="card-body">
-                                <h4 className="card-head fw-bolder">MAJOR REPAIR</h4>
+                                <h4 className="card-head fw-bolder">CAR INSPECTION</h4>
                                 <p className="card-text text-start">Get best Car Repair Service in anywhere in the Philippines.</p>
                                 <a href="">
                                     <button className="btn btn-danger fw-semibold view-service-btn">View Service <i className="bi bi-arrow-right fs-5"></i></button>
