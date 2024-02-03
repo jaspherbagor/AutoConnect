26450fc63b3c7b32064ace9c93f55c545b0bd327
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-03 20:05:44 +0800

    added a footer section

--- front-end/src/components/layouts/client/footer.jsx
+++ front-end/src/components/layouts/client/footer.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const ClientFooter = () => {
   return (
     <div>
-        <p>This is a footer from client</p>
+        <section className="footer container-fluid px-md-4 px-sm-4 px-2 py-5 bg-black">
+          
+        </section>
     </div>
   )
 }
