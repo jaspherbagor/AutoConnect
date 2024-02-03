95dab2b22f56c17cb4b9e434b6a5d3f1649f950f
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-03 19:31:27 +0800

    updated the description on the car detailing card

--- front-end/src/components/pages/client/home.jsx
+++ front-end/src/components/pages/client/home.jsx
@@ -139,7 +139,7 @@ const Home = () => {
                             <img src="../images/car_detailing.png" className="card-img-top img-fluid" alt="..."/>
                             <div className="card-body">
                                 <h4 className="card-head fw-bolder">CAR DETAILING</h4>
-                                <p className="card-text text-start">Get best Car Repair Service in anywhere in the Philippines.</p>
+                                <p className="card-text text-start">Indulge your car with our meticulous detailing services.</p>
                                 <a href="">
                                     <button className="btn btn-danger fw-semibold view-service-btn">View Service <i className="bi bi-arrow-right fs-5"></i></button>
                                 </a>
