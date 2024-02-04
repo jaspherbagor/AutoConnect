0bd15f53b7af80e8bfae83908dff7e7b332b305e
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-04 21:04:57 +0800

    added a footer link class to the footer links

--- front-end/src/components/layouts/client/footer.jsx
+++ front-end/src/components/layouts/client/footer.jsx
@@ -6,69 +6,71 @@ const ClientFooter = () => {
         <section className="footer container-fluid px-md-4 px-sm-4 px-2 py-5 bg-black">
           <div className="row">
             <div className="col-lg-2 col-md-4 col-sm-6 col-12 text-lg-start text-md-start text-sm-start text-center d-flex justify-content-center align-items-center">
-              <img src="../images/logo.svg" alt="" className="img-fluid" />
+              <a href="">
+                <img src="../images/logo.svg" alt="" className="img-fluid" />
+              </a>
             </div>
             <div className="col-lg-2 col-md-4 col-sm-6 col-12 text-lg-start text-md-start text-sm-start text-center text-white mb-4">
               <h5 className="fw-bolder mb-4">CUSTOMER SERVICE</h5>
-              <a href="" className="text-decoration-none text-white">
+              <a href="" className="text-decoration-none text-white footer-link">
                 <p className="footer-link">Contact Us</p>
               </a>
-              <a href="" className="text-decoration-none text-white">
-              <p className="footer-link">FAQs</p>
+              <a href="" className="text-decoration-none text-white footer-link">
+                <p className="footer-link">FAQs</p>
               </a>
-              <a href="" className="text-decoration-none text-white">
-              <p className="footer-link">Privacy Policy</p>
+              <a href="" className="text-decoration-none text-white footer-link">
+                <p className="footer-link">Privacy Policy</p>
               </a>
-              <a href="" className="text-decoration-none text-white">
-              <p className="footer-link">Terms & Condition</p>
+              <a href="" className="text-decoration-none text-white footer-link">
+                <p className="footer-link">Terms & Condition</p>
               </a>
             </div>
             <div className="col-lg-2 col-md-4 col-sm-6 col-12 text-lg-start text-md-start text-sm-start text-center text-white mb-4">
             <h5 className="fw-bolder mb-4">OUR SERVICES</h5>
-              <a href="" className="text-decoration-none text-white">
+              <a href="" className="text-decoration-none text-white footer-link">
                 <p className="footer-link">Denting & Painting</p>
               </a>
-              <a href="" className="text-decoration-none text-white">
-              <p className="footer-link">Major Repair</p>
+              <a href="" className="text-decoration-none text-white footer-link">
+                <p className="footer-link">Major Repair</p>
               </a>
-              <a href="" className="text-decoration-none text-white">
-              <p className="footer-link">Car Detailing</p>
+              <a href="" className="text-decoration-none text-white footer-link">
+                <p className="footer-link">Car Detailing</p>
               </a>
-              <a href="" className="text-decoration-none text-white">
-              <p className="footer-link">Battery Replacement</p>
+              <a href="" className="text-decoration-none text-white footer-link">
+                <p className="footer-link">Battery Replacement</p>
               </a>
             </div>
             <div className="col-lg-2 col-md-4 col-sm-6 col-12 text-lg-start text-md-start text-sm-start text-center text-white mb-4">
             <h5 className="fw-bolder mb-4">QUICK LINKS</h5>
-              <a href="" className="text-decoration-none text-white">
+              <a href="" className="text-decoration-none text-white footer-link">
                 <p className="footer-link">Home</p>
               </a>
-              <a href="" className="text-decoration-none text-white">
-              <p className="footer-link">About</p>
+              <a href="" className="text-decoration-none text-white footer-link">
+                <p className="footer-link">About</p>
               </a>
-              <a href="" className="text-decoration-none text-white">
-              <p className="footer-link">Services</p>
+              <a href="" className="text-decoration-none text-white footer-link">
+                <p className="footer-link">Services</p>
               </a>
             </div>
             <div className="col-lg-2 col-md-4 col-sm-6 col-12 text-lg-start text-md-start text-sm-start text-center text-white mb-4">
             <h5 className="fw-bolder mb-4">MY ACCOUNT</h5>
-              <a href="" className="text-decoration-none text-white">
+              <a href="" className="text-decoration-none text-white footer-link">
                 <p className="footer-link">Account</p>
               </a>
-              <a href="" className="text-decoration-none text-white">
-              <p className="footer-link">Purchases</p>
+              <a href="" className="text-decoration-none text-white footer-link">
+                <p className="footer-link">Purchases</p>
               </a>
             </div>
             <div className="col-lg-2 col-md-4 col-sm-6 col-12 text-lg-start text-md-start text-sm-start text-center text-white mb-4">
               <h5 className="fw-bolder">SOCIAL LINKS</h5>
               <div className="container-fluid d-flex align-items-center justify-content-lg-start justify-content-md-start justify-content-sm-start justify-content-center">
-                <a href="" className="text-decoration-none text-white">
+                <a href="" className="text-decoration-none text-white footer-link">
                   <i className="bi bi-facebook fs-3 me-3"></i>
                 </a>
-                <a href="" className="text-decoration-none text-white">
+                <a href="" className="text-decoration-none text-white footer-link">
                   <i className="bi bi-youtube fs-3 me-3"></i>
                 </a>
-                <a href="" className="text-decoration-none text-white">
+                <a href="" className="text-decoration-none text-white footer-link">
                   <i className="bi bi-linkedin fs-3"></i>
                 </a>               
               </div>
