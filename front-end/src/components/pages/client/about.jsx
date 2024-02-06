9e6dad539a16725f91b1d952f819e83d35d5e446
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-06 13:51:28 +0800

    imported the about css in the page

--- front-end/src/components/pages/client/about.jsx
+++ front-end/src/components/pages/client/about.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ClientNavbar from '../../layouts/client/navbar';
 import ClientFooter from '../../layouts/client/footer';
-
+import '../../styles/client/about.css'
 const About = () => {
   return (
     <div>
         <ClientNavbar/>
 
         <section className="vh-100">
-            <h2 className="mt-5 ">This is an about page</h2>
+            <h2>This is an about page</h2>
         </section>
 
         <ClientFooter/>
