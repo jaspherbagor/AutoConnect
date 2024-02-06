20a451c9d35b27ebf70c1bc4e9457d96c3a75774
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-06 13:43:16 +0800

    used the navbar and footer in the component

--- front-end/src/components/pages/client/about.jsx
+++ front-end/src/components/pages/client/about.jsx
@@ -4,10 +4,13 @@ import ClientFooter from '../../layouts/client/footer';
 const About = () => {
   return (
     <div>
+        <ClientNavbar/>
 
         <section>
             <h2>This is an about page</h2>
         </section>
+        
+        <ClientFooter/>
     </div>
   )
 }
