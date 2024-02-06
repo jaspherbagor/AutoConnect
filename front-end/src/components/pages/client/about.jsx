7395a7dfeba16613fcad06de7f4d133b615a8678
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-06 13:53:56 +0800

    updated the partial structure of the elements in the about page

--- front-end/src/components/pages/client/about.jsx
+++ front-end/src/components/pages/client/about.jsx
@@ -7,8 +7,8 @@ const About = () => {
     <div>
         <ClientNavbar/>
 
-        <section className="vh-100">
-            <h2>This is an about page</h2>
+        <section className="vh-100 px-md-4 px-sm-4 px-2 py-5">
+            <h2 className="pt-5 text-start">This is an about page... This is under construction...</h2>
         </section>
 
         <ClientFooter/>
