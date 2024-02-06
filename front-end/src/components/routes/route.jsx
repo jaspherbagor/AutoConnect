614933587fbd864a67482c40e05ff81620af973f
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-06 13:40:26 +0800

    added an about page in the route

--- front-end/src/components/routes/route.jsx
+++ front-end/src/components/routes/route.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import {Routes, Route} from "react-router-dom";
 import Home from '../pages/client/home';
+import About from '../pages/client/about';
 const RouteWay = () => {
   return (
     <div>
         <Routes>
             {/* Client Routes Start*/}
             <Route path="/" element={<Home/>} />
+            <Route path="/about" element={<About/>} />
             {/* Client Routes End */}
         </Routes>
     </div>
