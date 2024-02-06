0f5697839985691fa378d77aef2e025fc20685c1
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-06 13:45:20 +0800

    use and update link in the navigation bar

--- front-end/src/components/layouts/client/navbar.jsx
+++ front-end/src/components/layouts/client/navbar.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import '../../styles/client/navbar.css';
+import { Link } from 'react-router-dom';
 const ClientNavbar = () => {
     return (
         <div>
             <nav className="navbar navbar-expand-lg text-start px-2 py-1 position-fixed container-fluid">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">
+                    <Link className="navbar-brand" to="/">
                         <img src="../images/logo.svg" alt="" className="img-fluid" />
-                    </a>
+                    </Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active fw-semibold me-3 text-black" aria-current="page" href="#"><i className="bi bi-house-door fs-5"></i> HOME</a>
+                                <Link className="nav-link active fw-semibold me-3 text-black" aria-current="page" to="/"><i className="bi bi-house-door fs-5"></i> HOME</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link fw-semibold me-3 text-black" href="#"><i className="bi bi-info-circle fs-5"></i> ABOUT</a>
+                                <Link className="nav-link fw-semibold me-3 text-black" to="/about"><i className="bi bi-info-circle fs-5"></i> ABOUT</Link>
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link fw-semibold me-3 text-black" href="#"><i className="bi bi-tools fs-5"></i> SERVICES</a>
