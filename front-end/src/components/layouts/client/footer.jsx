266d362d27991e067f40998dfea5d639d4db95a1
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-07 17:43:51 +0800

    updated the home footer link target

--- front-end/src/components/layouts/client/footer.jsx
+++ front-end/src/components/layouts/client/footer.jsx
@@ -43,9 +43,9 @@ const ClientFooter = () => {
             </div>
             <div className="col-lg-2 col-md-4 col-sm-6 col-12 text-lg-start text-md-start text-sm-start text-center text-white mb-4">
             <h5 className="fw-bolder mb-4">QUICK LINKS</h5>
-              <a href="" className="text-decoration-none text-white footer-link">
+              <Link to="/" className="text-decoration-none text-white footer-link">
                 <p className="footer-link">Home</p>
-              </a>
+              </Link>
               <a href="" className="text-decoration-none text-white footer-link">
                 <p className="footer-link">About</p>
               </a>
