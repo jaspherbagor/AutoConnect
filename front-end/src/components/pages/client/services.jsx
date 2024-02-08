1394b3a5e2fc2a966f6fc575850636e70723c4a9
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-08 21:04:15 +0800

    added a temporary text display in the services page

--- front-end/src/components/pages/client/services.jsx
+++ front-end/src/components/pages/client/services.jsx
@@ -6,7 +6,9 @@ const Services = () => {
   return (
     <div>
         <ClientNavbar />
-
+            <section className="container-fluid px-md-4 px-sm-4 px-2 py-5">
+                <h2 className="pt-5">This page is for the services... This is under construction...</h2>
+            </section>
         <ClientFooter />
     </div>
   )
