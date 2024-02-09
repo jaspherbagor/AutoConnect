72afc7ba83d0e943fed646a91e18227727f7f592
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-09 19:21:37 +0800

    updated the terms and condition in the footer link

--- front-end/src/components/layouts/client/footer.jsx
+++ front-end/src/components/layouts/client/footer.jsx
@@ -22,9 +22,9 @@ const ClientFooter = () => {
               <a href="" className="text-decoration-none text-white footer-link">
                 <p className="footer-link">Privacy Policy</p>
               </a>
-              <a href="" className="text-decoration-none text-white footer-link">
+              <Link to="" className="text-decoration-none text-white footer-link">
                 <p className="footer-link">Terms & Condition</p>
-              </a>
+              </Link>
             </div>
             <div className="col-lg-2 col-md-4 col-sm-6 col-12 text-lg-start text-md-start text-sm-start text-center text-white mb-4">
             <h5 className="fw-bolder mb-4">OUR SERVICES</h5>
