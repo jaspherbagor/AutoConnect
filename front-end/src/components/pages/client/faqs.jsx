569c7ab548a31ef33db55d0c9a314332850d6c02
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-14 10:38:02 +0800

    added the navbar element on top of the content in the faq body

--- front-end/src/components/pages/client/faqs.jsx
+++ front-end/src/components/pages/client/faqs.jsx
@@ -4,6 +4,8 @@ import ClientFooter from '../../layouts/client/footer';
 const Faqs = () => {
   return (
     <div>
+        <ClientNavbar/>
+        
         <section className="container-fluid px-4 py-5">
             <h2 className="text-start">This is an FAQ page</h2>
         </section>
