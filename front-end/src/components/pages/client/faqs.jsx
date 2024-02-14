39dca84622d4e597845c3c786670d5eb594617a5
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-14 10:36:38 +0800

    added a partial content in the faq page

--- front-end/src/components/pages/client/faqs.jsx
+++ front-end/src/components/pages/client/faqs.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Faqs = () => {
   return (
-    <div>faqs</div>
+    <div>
+        <section className="container-fluid px-4 py-5">
+            <h2 className="text-start">This is an FAQ page</h2>
+        </section>
+    </div>
   )
 }
 
