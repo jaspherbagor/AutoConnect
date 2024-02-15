69bebf4ad938fee581d3ab21d54f64cb6262a6c0
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-15 20:12:42 +0800

    imported client navbar and footer in the contact page

--- front-end/src/components/pages/client/contact.jsx
+++ front-end/src/components/pages/client/contact.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import ClientNavbar from '../../layouts/client/navbar';
+import ClientFooter from '../../layouts/client/footer';
 
 const Contact = () => {
   return (
     <div>
         <section className="contact container-fluid px-4 py-5">
-            
+
         </section>
     </div>
   )
