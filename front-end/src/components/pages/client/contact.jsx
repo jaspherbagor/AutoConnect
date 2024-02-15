1277fb57451c307e443095472977c1010a320f82
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-15 20:14:44 +0800

    added a client footer in the contact page component

--- front-end/src/components/pages/client/contact.jsx
+++ front-end/src/components/pages/client/contact.jsx
@@ -10,6 +10,8 @@ const Contact = () => {
         <section className="contact container-fluid px-4 py-5">
             <h2 className="text-start">This is a contact page...</h2>
         </section>
+
+        <ClientFooter/>
     </div>
   )
 }
