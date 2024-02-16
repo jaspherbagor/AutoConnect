a400e2b81d7fab9b50b4745adc80b41a252e7d82
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-16 22:38:14 +0800

    updated the contact elements

--- front-end/src/components/pages/client/contact.jsx
+++ front-end/src/components/pages/client/contact.jsx
@@ -8,18 +8,18 @@ const Contact = () => {
       <ClientNavbar />
 
       <section className="contact-section container-fluid px-md-4 px-sm-4 px-3 py-5">
-        <h2 className="text-center pt-5">This is a contact page...</h2>
-        <div className="container">
+        <h2 className="text-center pt-5 fw-bolder">CONTACT FORM</h2>
+        <div className="container col-8 text-start">
           <form>
             <div className="mb-3">
-              <label for="exampleInputEmail1" className="form-label">Email address</label>
-              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+              <label htmlFor="email" className="form-label">Email address</label>
+              <input type="email" className="form-control" id="email" />
             </div>
             <div className="mb-3">
-              <label for="exampleInputPassword1" className="form-label">
+              <label htmlFor="exampleInputPassword1" className="form-label">
                 Password
               </label>
-              <input type="password" class="form-control" id="exampleInputPassword1"/>
+              <input type="password" className="form-control" id="exampleInputPassword1"/>
             </div>
             
             <button type="submit" className="btn btn-primary">
