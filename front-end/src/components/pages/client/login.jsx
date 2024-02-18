3fdaf8eabe71638e8bfb2c06c4c77724282d84bf
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-19 07:25:51 +0800

    added the navbar element in the component

--- front-end/src/components/pages/client/login.jsx
+++ front-end/src/components/pages/client/login.jsx
@@ -4,6 +4,8 @@ import ClientFooter from '../../layouts/client/footer';
 const Login = () => {
   return (
     <div>
+        <ClientNavbar/>
+
         <section className="container-fluid px-md-4 px-sm-4 px-3 py-5">
             <h2 className="fw-bolder text-start">This is a login page</h2>
         </section>
