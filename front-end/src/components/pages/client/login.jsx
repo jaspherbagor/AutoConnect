9349e1192c925f10022c49fb7c0ea40da38a75c9
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-19 07:19:04 +0800

    added a section in the login component

--- front-end/src/components/pages/client/login.jsx
+++ front-end/src/components/pages/client/login.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Login = () => {
   return (
     <div>
-        
+        <section className="container-fluid px-md-4 px-sm-4 px-3 py-5">
+            
+        </section>
     </div>
   )
 }
