141018510d2f328757763b81681c234eac694126
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-19 07:19:36 +0800

    added a temporary heading

--- front-end/src/components/pages/client/login.jsx
+++ front-end/src/components/pages/client/login.jsx
@@ -4,7 +4,7 @@ const Login = () => {
   return (
     <div>
         <section className="container-fluid px-md-4 px-sm-4 px-3 py-5">
-            
+            <h2 className="fw-bolder text-start">This is a login page</h2>
         </section>
     </div>
   )
