0571b39ae9b5bfa32fa996e44611abbc7d84389e
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-19 07:25:23 +0800

    imported the client navbar aand footer in the login page

--- front-end/src/components/pages/client/login.jsx
+++ front-end/src/components/pages/client/login.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import ClientNavbar from '../../layouts/client/navbar';
+import ClientFooter from '../../layouts/client/footer';
 const Login = () => {
   return (
     <div>
