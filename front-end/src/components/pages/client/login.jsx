065ea346457b0551447fab8284d311d2c1580118
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-20 09:11:20 +0800

    added an image in the contact container

--- front-end/src/components/pages/client/login.jsx
+++ front-end/src/components/pages/client/login.jsx
@@ -8,6 +8,7 @@ const Login = () => {
 
         <section className="container-fluid px-md-4 px-sm-4 px-3 py-5 d-flex align-items-center justify-content-center vh-100">
           <div className="login-container container bg-danger py-5 px-3">
+            <img src="../images/logo.svg" alt="logo" />
             <h2 className="fw-bolder text-center">LOGIN ACCOUNT</h2>
           </div>
         </section>
