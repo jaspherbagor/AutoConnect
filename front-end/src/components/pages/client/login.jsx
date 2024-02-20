e249d7f42a7ee2fb98d5533d07436e38cee5a7c5
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-20 09:14:17 +0800

    added a temporary structure of the form

--- front-end/src/components/pages/client/login.jsx
+++ front-end/src/components/pages/client/login.jsx
@@ -4,16 +4,34 @@ import ClientFooter from '../../layouts/client/footer';
 const Login = () => {
   return (
     <div>
-        <ClientNavbar/>
+      <ClientNavbar />
 
-        <section className="container-fluid px-md-4 px-sm-4 px-3 py-5 d-flex align-items-center justify-content-center">
-          <div className="login-container container bg-success py-5 mt-5 px-3">
-            <img src="../images/logo.svg" alt="logo" className="mb-3" />
-            <h2 className="fw-bolder text-center">LOGIN ACCOUNT</h2>
-          </div>
-        </section>
+      <section className="container-fluid px-md-4 px-sm-4 px-3 py-5 d-flex align-items-center justify-content-center">
+        <div className="login-container container bg-success py-5 mt-5 px-3">
+          <img src="../images/logo.svg" alt="logo" className="mb-3" />
+          <h2 className="fw-bolder text-center">LOGIN ACCOUNT</h2>
 
-        <ClientFooter/>
+          <form>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Email address</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputPassword1" class="form-label">Password</label>
+              <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3 form-check">
+              <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+          
+        </div>
+      </section>
+
+      <ClientFooter />
     </div>
   )
 }
