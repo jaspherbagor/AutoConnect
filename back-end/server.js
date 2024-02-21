2cc7d37d7d669b750156a0c2152c2f47acafeabd
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-21 14:56:01 +0800

    declared the connection details in the server

--- back-end/server.js
+++ back-end/server.js
@@ -6,9 +6,19 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+
+const connection = mysql.createConnection({
+    host: process.env.host,
+    port: process.env.port,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database
+});
 
 
 
 
-
-console.log(process.env.localhost);
+console.log(process.env.host);
+console.log(process.env.password)
