c2ba552be2b1da08745f3295f3ff0ff2f1ed6f3d
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-21 14:43:53 +0800

    added an initial content in the server

--- back-end/server.js
+++ back-end/server.js
@@ -1,4 +1,14 @@
+import mysql from "mysql";
+import express from "express";
+import cors from "cors";
+import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
+
+const app = express();
+
+
+
+
 
 console.log(process.env.localhost);
