f576a285e151144383bc09bd1f76ead3afecc68c
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-22 14:58:40 +0800

    added a temporary structure of the form

--- front-end/src/components/pages/client/register.jsx
+++ front-end/src/components/pages/client/register.jsx
@@ -4,14 +4,30 @@ import { Link } from 'react-router-dom';
 const Register = () => {
   return (
     <div>
-      <ClientNavbar/>
+      <ClientNavbar />
 
       <section className="register-section container-fluid px-md-4 px-sm-4 px-3 py-5 d-flex justify-content-center align-items-center">
-        <div className="container bg-success mt-5">
+        <div className="registration-container container bg-success mt-5">
           <Link to="/">
             <img src="../images/logo.svg" alt="logo" />
           </Link>
           <h2 className="text-center fw-bolder">REGISTER ACCOUNT</h2>
+          <form>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Email address</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputPassword1" class="form-label">Password</label>
+              <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3 form-check">
+              <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
         </div>
       </section>
     </div>
