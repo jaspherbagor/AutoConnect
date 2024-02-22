da2678fefa916efae38d6eccda2651ec7d25cfcb
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-22 19:43:31 +0800

    added a temporary structure of the register and a bootstrap toast

--- front-end/src/components/pages/client/register.jsx
+++ front-end/src/components/pages/client/register.jsx
@@ -7,7 +7,7 @@ const Register = () => {
     const passKey = document.getElementById('passKey');
     const togglePassKey = document.getElementById('togglePassKey');
 
-    if(passKey.type === "password") {
+    if (passKey.type === "password") {
       passKey.type = "text";
       togglePassKey.className = "bi bi-eye-slash fs-4 position-absolute";
     } else {
@@ -20,7 +20,7 @@ const Register = () => {
     const confirmPassword = document.getElementById('confirmPassword');
     const toggleConfirmPassword = document.getElementById('toggleConfirmPassword');
 
-    if(confirmPassword.type === "password") {
+    if (confirmPassword.type === "password") {
       confirmPassword.type = "text";
       toggleConfirmPassword.className = "bi bi-eye-slash fs-4 position-absolute"
     } else {
@@ -28,6 +28,12 @@ const Register = () => {
       toggleConfirmPassword.className = "bi bi-eye fs-4 position-absolute"
     }
   }
+
+  const register = () => {
+    const registerBtn = document.getElementById('register');
+    const
+  }
+
 
   return (
     <div>
@@ -42,29 +48,29 @@ const Register = () => {
           <form className="text-start">
             <div className="mb-3">
               <label htmlFor="name" className="form-label">Name</label>
-              <input type="text" className="form-control" id="name" name="name"/>
+              <input type="text" className="form-control" id="name" name="name" />
             </div>
             <div className="mb-3">
               <label htmlFor="username" className="form-label">Username</label>
-              <input type="text" className="form-control" id="username" name="username"/>
+              <input type="text" className="form-control" id="username" name="username" />
             </div>
             <div className="mb-3">
               <label htmlFor="email" className="form-label">Email</label>
-              <input type="email" className="form-control" id="email" name="email"/>
+              <input type="email" className="form-control" id="email" name="email" />
             </div>
             <div className="mb-3">
               <label htmlFor="passKey" className="form-label">Password</label>
               <div className="d-flex">
-                <input type="password" className="form-control" id="passKey" name="passKey"/>
+                <input type="password" className="form-control" id="passKey" name="passKey" />
                 <span className="view-password">
-                <i className="bi bi-eye fs-4 mt-1 position-absolute" id="togglePassKey" onClick={showPassword}></i>
+                  <i className="bi bi-eye fs-4 mt-1 position-absolute" id="togglePassKey" onClick={showPassword}></i>
                 </span>
               </div>
             </div>
             <div className="mb-3">
               <label htmlFor="confirmPassword" className="form-label">Confirm Password</label>
               <div className="d-flex">
-                <input type="password" className="form-control" id="confirmPassword" name="confirmPassword"/>
+                <input type="password" className="form-control" id="confirmPassword" name="confirmPassword" />
                 <span className="view-password">
                   <i className="bi bi-eye fs-4 mt-1 position-absolute" id="toggleConfirmPassword" onClick={showConfirmPassword}></i>
                 </span>
@@ -75,6 +81,16 @@ const Register = () => {
             </div>
             <p className="text-center mt-4">Already have an account? <Link to="/login" className="login-link text-decoration-none fw-semibold">Login</Link> instead.</p>
           </form>
+        </div>
+        <div className="toast-container position-fixed top-0 p-2">
+          <div id="liveToast" className="toast" role="alert" aria-live="assertive" aria-atomic="true">
+            <div className="toast-header" id="toastHeader">
+              <strong className="me-auto" id="toastLabel"></strong>
+              <button type="button" className="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+            <div className="toast-body fw-medium" id="toastMessage">
+            </div>
+          </div>
         </div>
       </section>
     </div>
