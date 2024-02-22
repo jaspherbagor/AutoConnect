3b8f8083668eba76268188d95560a9543f1bd707
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-22 14:26:01 +0800

    imported client navigation bar

--- front-end/src/components/pages/client/register.jsx
+++ front-end/src/components/pages/client/register.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import ClientNavbar from './../../layouts/client/navbar';
 const Register = () => {
   return (
     <div>
-        <h2 className="fw-bolder text-start mt-5">This is a registration page...</h2>
+
     </div>
   )
 }
