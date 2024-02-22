74be414cc0140915c742433802cecdaeef25b4a1
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-22 15:14:56 +0800

    updated the form

--- front-end/src/components/pages/client/register.jsx
+++ front-end/src/components/pages/client/register.jsx
@@ -7,31 +7,34 @@ const Register = () => {
       <ClientNavbar />
 
       <section className="register-section container-fluid px-md-4 px-sm-4 px-3 py-5 d-flex justify-content-center align-items-center">
-        <div className="registration-container container bg-success mt-5 py-4 px-3">
+        <div className="registration-container container bg-warning mt-5 py-4 px-3">
           <Link to="/">
             <img src="../images/logo.svg" alt="logo" />
           </Link>
           <h2 className="text-center fw-bolder mt-4 mb-3">REGISTER ACCOUNT</h2>
           <form className="text-start">
             <div className="mb-3">
-              <label for="name" className="form-label">Name</label>
+              <label htmlFor="name" className="form-label">Name</label>
               <input type="text" className="form-control" id="name" name="name"/>
             </div>
             <div className="mb-3">
-              <label for="exampleInputEmail1" className="form-label">Email address</label>
-              <input type="email" className="form-control" id="exampleInputEmail1"/>
+              <label htmlFor="username" className="form-label">Username</label>
+              <input type="text" className="form-control" id="username" name="username"/>
             </div>
             <div className="mb-3">
-              <label for="exampleInputEmail1" className="form-label">Email address</label>
-              <input type="email" className="form-control" id="exampleInputEmail1"/>
+              <label htmlFor="email" className="form-label">Email</label>
+              <input type="email" className="form-control" id="email" name="email"/>
             </div>
-
             <div className="mb-3">
-              <label for="exampleInputPassword1" className="form-label">Password</label>
-              <input type="password" className="form-control" id="exampleInputPassword1"/>
+              <label for="password" className="form-label">Password</label>
+              <input type="password" className="form-control" id="password" name="password"/>
+            </div>
+            <div className="mb-3">
+              <label for="confirmPassword" className="form-label">Confirm Password</label>
+              <input type="password" className="form-control" id="confirmPassword" name="confirmPassword"/>
             </div>
             <div className="text-center">
-              <button type="submit" className="btn btn-primary">Submit</button>
+              <button type="submit" className="btn btn-success">Submit</button>
             </div>
           </form>
         </div>
