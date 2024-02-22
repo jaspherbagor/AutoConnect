d3714798af1d9bfc59c30cfbb278c48455fed9e3
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-22 14:31:04 +0800

    added a container and heading for the registration form

--- front-end/src/components/pages/client/register.jsx
+++ front-end/src/components/pages/client/register.jsx
@@ -5,8 +5,11 @@ const Register = () => {
     <div>
       <ClientNavbar/>
 
-      <section className="register-section container-fluid px-md-4 px-sm-4 px-3 py-5">
-        
+      <section className="register-section container-fluid px-md-4 px-sm-4 px-3 py-5 d-flex justify-content-center align-items-center">
+        <div className="container-fluid">
+          
+          <h2 className="text-center fw-bolder"></h2>
+        </div>
       </section>
     </div>
   )
