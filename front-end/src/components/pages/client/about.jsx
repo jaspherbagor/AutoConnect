b0e8394a51e98f2f88685678056b67221ba9059a
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-28 20:06:00 +0800

    added a heading on the about page

--- front-end/src/components/pages/client/about.jsx
+++ front-end/src/components/pages/client/about.jsx
@@ -7,8 +7,13 @@ const About = () => {
     <div>
         <ClientNavbar/>
 
-        <section className="vh-100 px-md-4 px-sm-4 px-2 py-5">
-            <h2 className="pt-5 text-start">This is an about page... This is under construction...</h2>
+        <section className="container-fluid vh-100">
+            <div className="about-head container-fluid d-flex align-items-center justify-content-center py-5 bg-danger vw-100">
+              <h2 className="text-center fw-bolder pt-4 pb-3">ABOUT AUTO CONNECT</h2>
+            </div>
+            <div className="about-content container-fluid px-md-4 px-sm-4 px-2 py-5">
+
+            </div>
         </section>
 
         <ClientFooter/>
