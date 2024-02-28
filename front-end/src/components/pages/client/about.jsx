09e554327ef84aa45088ecc0d9c3d555b2815130
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-28 20:08:41 +0800

    added a top margin on the heading

--- front-end/src/components/pages/client/about.jsx
+++ front-end/src/components/pages/client/about.jsx
@@ -9,7 +9,7 @@ const About = () => {
 
         <section className="container-fluid vh-100">
             <div className="about-head container-fluid d-flex align-items-center justify-content-center py-5 bg-danger vw-100">
-              <h2 className="text-center fw-bolder pt-4 pb-3">ABOUT AUTO CONNECT</h2>
+              <h2 className="text-center fw-bolder pt-4 pb-3 mt-5">ABOUT AUTO CONNECT</h2>
             </div>
             <div className="about-content container-fluid px-md-4 px-sm-4 px-2 py-5">
 
