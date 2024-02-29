c76827165e2721721e75adfceeebde1ecc3a71ef
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-02-29 21:12:28 +0800

    nested the header inside the div

--- front-end/src/components/pages/client/home.jsx
+++ front-end/src/components/pages/client/home.jsx
@@ -155,8 +155,9 @@ const Home = () => {
 
             </section>
             <section className="newsletter-subscription-section container-fluid px-md-4 pxx-sm-4 px-3 py-5">
-                
-                <h2 className="text-center fw-bolder">Subscribe to AUTOCONNECT's Latest Promotion</h2>
+                <div className="container">
+                    <h2 className="text-center fw-bolder">Subscribe to AUTOCONNECT's Latest Promotion</h2>
+                </div>
             </section>
             <section className="customers-review container-fluid px-md-4 px-sm-4 px-3 py-5">
                 <h2 className="fw-bolder text-center text-white"><span>CUSTOMERS</span> REVIEW</h2>
