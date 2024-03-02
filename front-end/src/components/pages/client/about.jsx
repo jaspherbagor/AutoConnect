2f3e026acaf399388993cffde36c20462ae5c3d0
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-03-02 20:12:10 +0800

    temporrily updated the contents in the about page

--- front-end/src/components/pages/client/about.jsx
+++ front-end/src/components/pages/client/about.jsx
@@ -7,11 +7,9 @@ const About = () => {
     <div>
         <ClientNavbar/>
 
-        <section className="container-fluid w-100">
-            <div className="about-head container-fluid d-flex align-items-center justify-content-center py-5 bg-danger w-100">
-              <h2 className="text-center fw-bolder pt-4 pb-3 mt-5">ABOUT AUTO CONNECT</h2>
-            </div>
-            <div className="about-content container-fluid px-md-4 px-sm-4 px-2 py-5">
+        <section className="about-section container-fluid px-md-4 px-sm-4 px-3 py-5">
+            <h2 className="fw-bolder text-center mt-5 pt-4">ABOUT AUTOCONNECT</h2>
+            <div className="about-content">
               <h2>This is an about page content</h2>
             </div>
         </section>
