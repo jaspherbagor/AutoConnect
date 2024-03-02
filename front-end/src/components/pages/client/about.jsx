47074039a94e2442e937eb62998a94745bc3d4d8
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-03-02 20:15:31 +0800

    added an about text

--- front-end/src/components/pages/client/about.jsx
+++ front-end/src/components/pages/client/about.jsx
@@ -9,6 +9,7 @@ const About = () => {
 
         <section className="about-section container-fluid px-md-4 px-sm-4 px-3 py-5">
             <h2 className="fw-bolder text-center mt-5 pt-4">ABOUT AUTOCONNECT</h2>
+            <p className="text-center lead mt-5">Established in November of the previous year by founder Ronaldo Tarroza, AUTO CONNECT was born out of a vision to revolutionize the car service industry in the Philippines. With a mission to simplify the process for car owners in need of quality mechanics, AUTO CONNECT introduces a groundbreaking approach to automotive care through its Doorstep - Pickup & Drop service.</p>
             <div className="about-content">
               <h2>This is an about page content</h2>
             </div>
