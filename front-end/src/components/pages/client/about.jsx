fb708a8e7fe501ea8b24fcf5b125d3f71ca3798d
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-03-02 20:27:31 +0800

    updated the about lead content

--- front-end/src/components/pages/client/about.jsx
+++ front-end/src/components/pages/client/about.jsx
@@ -9,7 +9,7 @@ const About = () => {
 
         <section className="about-section container-fluid px-md-4 px-sm-4 px-3 py-5">
             <h2 className="fw-bolder text-center mt-5 pt-4">ABOUT AUTOCONNECT</h2>
-            <p className="text-center lead mt-5">Established in November of the previous year by founder Ronaldo Tarroza, AUTO CONNECT was born out of a vision to revolutionize the car service industry in the Philippines. With a mission to simplify the process for car owners in need of quality mechanics, AUTO CONNECT introduces a groundbreaking approach to automotive care through its Doorstep - Pickup & Drop service.</p>
+            <p className="text-center lead mt-5">Founded in November 2023 by visionary entrepreneur Ronaldo Tarroza, AUTO CONNECT stands as a testament to a commitment to redefine the landscape of the automotive service industry in the Philippines. Our core mission is rooted in the desire to streamline the experience for discerning car owners seeking top-notch mechanical expertise. AUTO CONNECT introduces a pioneering methodology to automotive care with its innovative Doorstep - Pickup & Drop service.</p>
             <div className="about-content">
               <h2>This is an about page content</h2>
             </div>
