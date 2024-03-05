ba4f377533a67d2b1e081c358fa83683bf36c272
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-03-05 19:42:16 +0800

    updated the accordion item 4

--- front-end/src/components/pages/client/faqs.jsx
+++ front-end/src/components/pages/client/faqs.jsx
@@ -49,12 +49,12 @@ const Faqs = () => {
             <div class="accordion-item">
               <h2 class="accordion-header">
                 <button class="accordion-button collapsed fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#faq-Four" aria-expanded="true" aria-controls="faq-Four">
-                  Accordion Item #1
+                  4. What sets AUTOCONNECT apart from traditional automotive service providers?
                 </button>
               </h2>
               <div id="faq-Four" class="accordion-collapse collapse show">
                 <div class="accordion-body text-start">
-                  <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                  <p>AUTOCONNECT distinguishes itself through the innovative Doorstep - Pickup & Drop service, bringing the workshop to your doorstep. This eliminates the hassle of traditional service models, offering unmatched convenience.</p>
                 </div>
               </div>
             </div>
