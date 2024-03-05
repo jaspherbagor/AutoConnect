d33341659949829bdf1e6daa9d4e3a6b5039e5f7
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-03-05 19:48:03 +0800

    updated the accordion item 9

--- front-end/src/components/pages/client/faqs.jsx
+++ front-end/src/components/pages/client/faqs.jsx
@@ -113,13 +113,13 @@ const Faqs = () => {
 
             <div class="accordion-item">
               <h2 class="accordion-header">
-                <button class="accordion-button fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#faq-Nine" aria-expanded="true" aria-controls="faq-Nine">
-                  Accordion Item #1
+                <button class="accordion-button collapsed fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#faq-Nine" aria-expanded="true" aria-controls="faq-Nine">
+                  9. Do you offer any warranty on your services?
                 </button>
               </h2>
               <div id="faq-Nine" class="accordion-collapse collapse show">
                 <div class="accordion-body text-start">
-                  <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                  <p>Yes, we stand by the quality of our services. AUTOCONNECT provides a warranty on certain repairs and services. Details regarding warranties will be discussed during the service booking and consultation process.</p>
                 </div>
               </div>
             </div>
