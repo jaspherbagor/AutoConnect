c28fca53a363003b8b9dec0be64fe99cf0154a0b
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-03-05 19:38:21 +0800

    updated the accordion item 1

--- front-end/src/components/pages/client/faqs.jsx
+++ front-end/src/components/pages/client/faqs.jsx
@@ -12,13 +12,13 @@ const Faqs = () => {
           <div class="accordion" id="accordionPanelsStayOpenExample">
             <div class="accordion-item">
               <h2 class="accordion-header">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#faq-One" aria-expanded="true" aria-controls="faq-One">
-                  Accordion Item #1
+                <button class="accordion-button fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#faq-One" aria-expanded="true" aria-controls="faq-One">
+                  1. How does the Doorstep - Pickup & Drop service work?
                 </button>
               </h2>
               <div id="faq-One" class="accordion-collapse collapse show">
                 <div class="accordion-body text-start">
-                  <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                  <p>Our Doorstep service is designed for your convenience. Schedule a pickup, and our team will collect your vehicle, perform the necessary services, and return it to your doorstep—all without you having to visit the workshop.</p>
                 </div>
               </div>
             </div>
