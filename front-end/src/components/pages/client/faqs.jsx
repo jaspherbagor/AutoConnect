af060f53b88cd2c389f4f4b4c4d00e8746e9162b
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-03-05 19:49:09 +0800

    updated the accordion item 10

--- front-end/src/components/pages/client/faqs.jsx
+++ front-end/src/components/pages/client/faqs.jsx
@@ -126,13 +126,13 @@ const Faqs = () => {
 
             <div class="accordion-item">
               <h2 class="accordion-header">
-                <button class="accordion-button fw-medium" type="button" data-bs-toggle="collapse" data-bs-target="#faq-Ten" aria-expanded="true" aria-controls="faq-Ten">
-                  Accordion Item #1
+                <button class="accordion-button collapsed fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#faq-Ten" aria-expanded="true" aria-controls="faq-Ten">
+                  10. How can I provide feedback or address concerns about my service experience?
                 </button>
               </h2>
               <div id="faq-Ten" class="accordion-collapse collapse show">
                 <div class="accordion-body text-start">
-                  <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                  <p>We value your feedback! You can provide your thoughts through our online feedback form on the website, or you can contact our customer service directly. We are committed to continuous improvement and ensuring your satisfaction.</p>
                 </div>
               </div>
             </div>
