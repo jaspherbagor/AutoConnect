aff5516f872bc570ee1ec79fb4b2b36a848ec9e2
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-03-06 19:06:23 +0800

    added a paragraph below the header

--- front-end/src/components/pages/client/services.jsx
+++ front-end/src/components/pages/client/services.jsx
@@ -7,8 +7,8 @@ const Services = () => {
     <div>
         <ClientNavbar />
             <section className="container-fluid px-md-4 px-sm-4 px-3 py-5">
-                <h2 className="pt-5 fw-bolder text-center">SERVICES</h2>
-                
+              <h2 className="pt-5 fw-bolder text-center">SERVICES</h2>
+              <p className="text-center">Doorstep - Pickup & Drop</p>
             </section>
         <ClientFooter />
     </div>
