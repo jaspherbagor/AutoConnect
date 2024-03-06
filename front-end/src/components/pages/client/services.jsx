82aed08eb5710011ae180146412f030161134931
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-03-06 19:17:22 +0800

    added a raw structure of the card

--- front-end/src/components/pages/client/services.jsx
+++ front-end/src/components/pages/client/services.jsx
@@ -9,7 +9,15 @@ const Services = () => {
             <section className="container-fluid px-md-4 px-sm-4 px-3 py-5">
               <h2 className="pt-5 fw-bolder text-center">SERVICES</h2>
               <p className="text-center">Doorstep - Pickup & Drop</p>
-              <div className="row d-flex align-items-center justify-content-center">
+              <div className="row d-flex align-items-center justify-content-center mt-5">
+                <div className="col-lg-3 col-md-4 col-sm-4 col-12">
+                  <div className="card">
+                    <img src="../images/car_detailing.png" class="card-img-top img-fluid" alt="..."/>
+                    <div className="card-body">
+                      <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                  </div>
+                </div>
                 <div className="col-lg-3 col-md-4 col-sm-4 col-12">
 
                 </div>
