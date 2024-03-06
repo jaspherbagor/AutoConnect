e28293cdeae693d4016e8123cfc8f469ae1d8a58
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-03-06 19:13:16 +0800

    added a structure of the row columns for the services

--- front-end/src/components/pages/client/services.jsx
+++ front-end/src/components/pages/client/services.jsx
@@ -9,6 +9,17 @@ const Services = () => {
             <section className="container-fluid px-md-4 px-sm-4 px-3 py-5">
               <h2 className="pt-5 fw-bolder text-center">SERVICES</h2>
               <p className="text-center">Doorstep - Pickup & Drop</p>
+              <div className="row d-flex align-items-center justify-content-center">
+                <div className="col-lg-3 col-md-4 col-sm-4 col-12">
+
+                </div>
+                <div className="col-lg-3 col-md-4 col-sm-4 col-12">
+
+                </div>
+                <div className="col-lg-3 col-md-4 col-sm-4 col-12">
+                  
+                </div>
+              </div>
             </section>
         <ClientFooter />
     </div>
