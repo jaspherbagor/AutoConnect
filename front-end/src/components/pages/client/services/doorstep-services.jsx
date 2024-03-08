49fe894b6d45c08f700c0141cad856b5f11feecd
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-03-08 14:20:45 +0800

    added a heading to the doorstep services page

--- front-end/src/components/pages/client/services/doorstep-services.jsx
+++ front-end/src/components/pages/client/services/doorstep-services.jsx
@@ -3,7 +3,13 @@ import ClientNavbar from './../../../layouts/client/navbar';
 import ClientFooter from '../../../layouts/client/footer';
 const doorstepServices = () => {
   return (
-    <div>doorstep-services</div>
+    <div>
+        <ClientNavbar/>
+        <section className="container-fluid px-md-4 px-sm-4 px-3 py-5">
+            <h2 className="fw-bolder text-center">Doorstep Services</h2>
+        </section>
+        <ClientFooter/>
+    </div>
   )
 }
 
