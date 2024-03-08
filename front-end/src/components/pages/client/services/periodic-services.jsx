10a832a0db66f9375ed7d5e8b77fa1a7cd52e630
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-03-08 14:13:32 +0800

    updated the elements of the periodic services page

--- front-end/src/components/pages/client/services/periodic-services.jsx
+++ front-end/src/components/pages/client/services/periodic-services.jsx
@@ -4,9 +4,11 @@ import ClientFooter from '../../../layouts/client/footer';
 const periodicServices = () => {
   return (
     <div>
+        <ClientNavbar/>
         <section className="container-fluid px-md-4 px-sm-4 px-3 py-5">
-            
+            <h2 className="fw-bolder text-center">Periodic Services</h2>
         </section>
+        <ClientFooter/>
     </div>
   )
 }
