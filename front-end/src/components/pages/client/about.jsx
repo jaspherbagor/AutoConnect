9fd5474ff9f7d864f1eb68523d45c27eed94480b
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-03-11 15:50:18 +0800

    added an emphasis to the name of the founder

--- front-end/src/components/pages/client/about.jsx
+++ front-end/src/components/pages/client/about.jsx
@@ -11,7 +11,7 @@ const About = () => {
 
             <h2 className="fw-bolder text-center mt-5 pt-4">ABOUT <span>AUTOCONNECT</span></h2>
             <div className="col-md-10 col-sm-10 col-12 justify-content-center align-items-center d-flex container">
-              <p className="text-center lead mt-4">Founded in November 2023 by visionary entrepreneur Ronaldo Tarroza, AUTO CONNECT stands as a testament to a commitment to redefine the landscape of the automotive service industry in the Philippines. Our core mission is rooted in the desire to streamline the experience for discerning car owners seeking top-notch mechanical expertise. AUTO CONNECT introduces a pioneering methodology to automotive care with its innovative Doorstep - Pickup & Drop service.</p>
+              <p className="text-center lead mt-4">Founded in November 2023 by visionary entrepreneur <strong>Ronaldo Tarroza</strong>, AUTO CONNECT stands as a testament to a commitment to redefine the landscape of the automotive service industry in the Philippines. Our core mission is rooted in the desire to streamline the experience for discerning car owners seeking top-notch mechanical expertise. AUTO CONNECT introduces a pioneering methodology to automotive care with its innovative Doorstep - Pickup & Drop service.</p>
             </div>
            
 
